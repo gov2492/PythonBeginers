14074f1a3acf417ea7bce6143474759ded1572aa
gov2492 <35134455+gov2492@users.noreply.github.com>
2025-08-23 19:41:17 +0530

    variables

---
+++ Variables/VariableExample.py
@@ -0,0 +1,47 @@
+# name = "Govind"
+# age = 20
+# print(name, age)
+# lastName = "Kumar"
+# age = 24
+# is_adult = True
+# print (name+lastName)
+# print(name,age, is_adult)
+
+# name = input("what is your name?")
+# print("Hello " + name)
+
+# superHero = input("What is your superhero?")
+# print(superHero)
+
+### sum of two number ######
+# firstNum = input("enter first Number:")
+# secondNum = input("enter second Number:")
+# sum = int(firstNum) + int(secondNum)
+# print("Sum is :" + str(sum))
+name = "Govind Kumar"
+print(name.upper())
+print(name)
+print(name.lower())
+print(name.find('Ku'))
+print(name.replace('Kumar', 'Singh'))
+print('G' in name)
+print('g' in name)
+print('Kumar' in name)
+
+###### Arithmetic operation ############
+print(5+3)
+print(5-3)
+print(5*3)
+print(5/3)
+print(5//3) # get only integer
+print(5%3) # get reminder
+print(5**3) ## power
+
+i= 5;
+i+=2;
+print(i)
+
+##### Operator precedence #########
+#### Just Like follow as mathematics#######
+result = 2+3*5
+print(result)
